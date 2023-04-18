3598b386f95790858ace1001bd793810393cabb2
Ksenia Fihas <ksenia.fihas@gmail.com>
2023-04-18 17:39:06 +0200

    функції для обчислення відгуків

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,38 @@
+import { Component } from 'react';
+import { Feedback } from './Feedback/Feedback';
+
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  };
+
+
+  updateState = nameFeedback => {
+    this.setState(oldData => {
+      let obj = { ...oldData };
+
+
+      obj[nameFeedback] = oldData[nameFeedback] + 1;
+      return obj;
+    });
+  };
+
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return Math.floor(
+      ((this.state.good) / (this.state.good + this.state.neutral + this.state.bad))*100 || 0
+    );
+  };
+
+
+
+
 export const App = () => {
   return (
     <div
@@ -10,7 +45,7 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      
     </div>
   );
 };
